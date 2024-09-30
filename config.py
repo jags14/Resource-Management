a9637f8e99b4997c0111912c8cb1bd56a7f1b9ba
Jagpat Singh <jsingh14192@gmail.com>
2024-09-30 23:25:20 +0530

    modified config

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ class Config(object):
 class DevelopmentConfig():
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+    SECRET_KEY = 'alwaysdaring_14'
+    SECURITY_PASSWORD_SALT = "thisissalt"
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
+    WTF_CSRF_ENABLED = False
+    SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
