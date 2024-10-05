f54cdd926f6451fe1c1cb757ed4f1a426c4163e6
Jagpat Singh <jsingh14192@gmail.com>
2024-10-05 23:59:15 +0530

    kk

--- upload_initial_data.py
+++ upload_initial_data.py
@@ -1,28 +1,25 @@
 from main import app, datastore
 from application.models import db, RoleModel
-from flask_security import UserDatastore
+from flask_security import hash_password
 
 with app.app_context():
     # this code will have access to application level data
     db.create_all()
-    # creates all tables in the database
-    # admin = RoleModel(id='admin', name='Admin', description='admin description')
-    # db.session.add(admin)
-    # student = RoleModel(id='student', name='Student', description='student description')
-    # db.session.add(student)
-    # instructor = RoleModel(id='instructor', name='Instructor', description='Instructor description')
-    # db.session.add(instructor)
-    # db.session.commit()
+
     datastore.find_or_create_role(name='admin', description='User is the admin')
     datastore.find_or_create_role(name='inst', description='User is an instructor')
     datastore.find_or_create_role(name='stud', description='User is an Student')
     db.session.commit()
 
     if not datastore.find_user(email='admin@gmail.com'):
-        datastore.create_user(email='admin@gmail.com', password='admin', roles=['admin'])
+        datastore.create_user(username='adminuser', email='admin@gmail.com', password=hash_password("admin"), roles=['admin'])
+        db.session.commit()
     if not datastore.find_user(email='instruct1@gmail.com'):
-        datastore.create_user(email='instruct1@gmail.com', password='instruct1', roles=['inst'])
+        datastore.create_user(username='instructoruser', email='instruct1@gmail.com', password=hash_password("instruct1"), roles=['inst'])
+        db.session.commit()
     if not datastore.find_user(email='student1@gmail.com'):
-        datastore.create_user(email='student1@gmail.com', password='student1', roles=['stud'])
-
+        datastore.create_user(username='studentuser', email='student1@gmail.com', password=hash_password("student1"), roles=['stud'])
+        db.session.commit()
+    
+        
 
