76b48f6233f72cf9a97faa4a5b13b7c6aea813e1
Jagpat Singh <jsingh14192@gmail.com>
2024-10-08 23:35:50 +0530

    protected views

--- application/views.py
+++ application/views.py
@@ -1,6 +1,12 @@
 from flask import current_app as app
+from flask_security import auth_required, roles_required
 
 @app.get('/')
 def home():
     return 'Hello World'
 
+@app.get('/admin')
+@auth_required('token')
+@roles_required('admin')
+def admin():
+    return "welcome, admin"
